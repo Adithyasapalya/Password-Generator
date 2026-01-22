9dfe4fe96ad8fc84442c339f6d1c4a78fa338eef
Adithya Sapalya <adithyasapalya30@gmail.com>
2026-01-22 20:25:25 +0530

    added letters, digits, ounc

--- pg.py
+++ pg.py
@@ -2,4 +2,9 @@ import random
 import string
 
 def generate_password(min_len, numbers=True, special_chars=True):
-    pass
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+
+generate_password(10)
